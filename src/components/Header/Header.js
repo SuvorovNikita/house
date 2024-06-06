88e49355686ffb08bcfdfe9044c7f6800bcfac1c
NS <nikit.suvorov@gmail.com>
2024-06-06 11:46:07 +0300

    add mobile menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,19 +6,19 @@ import Image from 'next/image';
 import '../Header/Header.scss';
 
 import logo from '@/assets/img/logo.svg';
-// import cart from '@/assets/img/cart.svg';
-// import menu from '@/assets/img/menu.svg';
-// import close from '@/assets/img/close.svg';
+import cart from '@/assets/img/cart.svg';
+import menu from '@/assets/img/menu.svg';
+import close from '@/assets/img/close.svg';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
+  const handleMenuClick = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const closeMenu = () => {
-    setIsMenuOpen(true);
+  const handleMenuClose = () => {
+    setIsMenuOpen(false);
   };
 
   return (
@@ -31,10 +31,7 @@ const Header = () => {
             </Link>
           </div>
           <div className="header__right">
-            <nav
-              className={`nav header__nav ${isMenuOpen ? 'header__nav--active' : ''}`}
-              onClick={closeMenu}
-            >
+            <nav className="nav">
               <ul className="list-reset nav__list">
                 <li className="nav__item">
                   <Link href="#catalog" className="nav__link">
@@ -53,13 +50,6 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
-            <button
-              className={`burger ${isMenuOpen ? 'burger--active' : ''}`}
-              aria-label="Открыть меню"
-              onClick={toggleMenu}
-            >
-              <span className="burger__line"></span>
-            </button>
             <Link href="#" className="cart" aria-label="Переход в корзину">
               <svg
                 width="24"
@@ -96,6 +86,53 @@ const Header = () => {
               </svg>
             </Link>
           </div>
+
+          <nav className="mobile-menu">
+            <div className="mobile-menu__inner">
+              <div className="header-menu__left">
+                <Link className="mobile-menu__button" href="#" onClick={handleMenuClick}>
+                  <Image src={menu} alt="menu" />
+                </Link>
+                {isMenuOpen && (
+                  <div className="mobile-menu__overlay">
+                    <div
+                      className={`mobile-menu__content ${
+                        isMenuOpen ? 'mobile-menu__content--visible' : ''
+                      }`}
+                    >
+                      <ul className="mobile-menu__list">
+                        <li className="mobile-menu__item">
+                          <Link className="mobile-menu__link" href="#" onClick={handleMenuClose}>
+                            <Image src={close} alt="Закрыть меню" />
+                          </Link>
+                        </li>
+                        <li className="mobile-menu__item">
+                          <Link className="mobile-menu__link" href="#">
+                            Репродукции
+                          </Link>
+                        </li>
+                        <li className="mobile-menu__item">
+                          <Link className="mobile-menu__link" href="#">
+                            Новинки
+                          </Link>
+                        </li>
+                        <li className="mobile-menu__item">
+                          <Link className="mobile-menu__link" href="#">
+                            О Нас
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className="header-menu__right">
+                <Link className="mobile-menu__button" href="#" aria-label="Переход в корзину">
+                  <Image src={cart} alt="cart" />
+                </Link>
+              </div>
+            </div>
+          </nav>
         </div>
       </div>
     </header>
