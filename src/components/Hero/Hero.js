f81e535e9d5761c3b2fb7adececbd9a15b2d6679
NS <nikit.suvorov@gmail.com>
2024-06-07 10:24:49 +0300

    adaptive image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,6 +6,7 @@ import '../Button/Button.scss';
 
 import smallImage from '../../assets/img/small-image.png';
 import tableImage from '../../assets/img/mid-image.png';
+import lgImage from '../../assets/img/lg-image.png';
 
 const Hero = () => {
   return (
@@ -14,7 +15,8 @@ const Hero = () => {
         <div className="hero__inner">
           <div className="hero__image">
             <Image className="image__small-image" src={smallImage} alt="" />
-            {/* <Image className="image__mid-image" src={tableImage} alt="" /> */}
+            <Image className="image__mid-image" src={tableImage} alt="" />
+            <Image className="image__lg-image" src={lgImage} alt="" />
           </div>
           <div className="hero__content">
             <h1 className="hero__title">
